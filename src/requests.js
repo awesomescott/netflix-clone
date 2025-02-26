49645d2f34cdfc414f6c7784b09dd139c0b22efe
Scott Gilmore <scott@plsol.com>
2025-02-26 14:51:58 -0500

    Add env var

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "00dd398961728bc27bca367d5d9f10d4";
+const API_KEY = process.env.API_KEY;
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
